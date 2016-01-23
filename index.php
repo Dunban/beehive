1048fe0e626cde2fbb0f64ea9b4ee8246e1a6445
Dunban <sevenson2@mail.greenriver.edu>
2016-01-22 16:49:04 -0800

    Version 1.1.4: PHPEXCEL addition for downloading data

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Beehive Mite Count Display</title>
+    </head>
+    <body>
+        <a href="Views/mite_Display.php">Form</a>
+        <a href="Views/admin.php">Admin</a>
+    </body>
+</html>
